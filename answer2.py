cb1283c0f7b099ec57e2c74f3ed5ffd83fc02011
Iván Olmedo Cortés <alcory@users.noreply.github.com>
2022-12-03 22:06:36 +0100

    Add files via upload

---
+++ answer2.py
@@ -0,0 +1,16 @@
+with open('file.in') as file:
+    data = [i for i in file.read().strip().split("\n")]
+
+sum = 0
+resultList = []
+for i in data:
+  if i != "":
+    sum = sum + int(i)
+  if i == "":
+    resultList.append(sum)
+    sum = 0
+
+resultList.sort(reverse=True)
+
+myresult = resultList[0] + resultList[1] + resultList[2]
+print(myresult)
